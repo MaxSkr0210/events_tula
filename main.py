36a0528e494e3c399b355c361e8c7a966172bab1
MaxSkr0210 <maxskr0210@mail.ru>
2023-09-29 17:25:25 +0300

    add main.py
    add main.py

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from datetime import datetime
-import os
 import sqlite3
 
-import aiogram_dialog.widgets.input.base
-from aiogram import Bot, Dispatcher, executor
-
-from aiogram.types import CallbackQuery
+from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.types import CallbackQuery, ReplyKeyboardRemove
+from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.types import Message
+from aiogram_dialog import DialogRegistry, DialogManager, Window, Dialog
+from aiogram_dialog.widgets.kbd import Next, Button, Cancel
+from aiogram_dialog.widgets.text import Const
+from aiogram_dialog.widgets.input import MessageInput
 
 from aiogram_dialog import Window, Dialog, DialogRegistry, DialogManager, StartMode
 
-from aiogram_dialog.widgets.input import MessageInput
-from aiogram_dialog.widgets.kbd import Next, Button, Cancel
-from aiogram_dialog.widgets.text import Const
 
-storage = MemoryStorage()
 bot = Bot(token='6325210305:AAGZSFpDyFn2bfuH8jujhs6I_Ms5Pa9ZwTo')
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+dp.middleware.setup(LoggingMiddleware())
+
 registry = DialogRegistry(dp)
-
-event_array = []
-
 
 class EventRegistrationForm(StatesGroup):
     start = State()
@@ -38,9 +39,9 @@ class EventRegistrationForm(StatesGroup):
 
 
 async def insert_event(event_arr):
-    global event_array
-    connection = sqlite3.connect('/home/intensa/database_dir/tulaevents.db')
     try:
+        connection = sqlite3.connect('/home/intensa/database_dir/tulaevents.db')
+
         sql_query = """
             INSERT INTO events (event_name, description, age_restrictions, company, category, 
                                 start_date, end_date, address, need_pre_reg, is_registered)
@@ -48,35 +49,66 @@ async def insert_event(event_arr):
         """
 
         connection.execute(sql_query, event_arr)
-
-        event_array = []
-
         connection.commit()
+        connection.close()
     except sqlite3.Error as e:
         print(e)
-    finally:
-        connection.close()
 
 
-async def on_input(m: Message, dialog: Dialog, manager: DialogManager):
-    manager.current_context().dialog_data["name"] = m.text
-    event_array.append(m.text)
+async def on_input(m: types.Message, dialog: Dialog, manager: DialogManager):
+    current_state = manager.current_context().state
+    user_data = manager.current_context().dialog_data
+    if current_state == EventRegistrationForm.event_name:
+        user_data["event_name"] = m.text
+    elif current_state == EventRegistrationForm.description:
+        user_data["description"] = m.text
+    elif current_state == EventRegistrationForm.age_restrictions:
+        user_data["age_restrictions"] = m.text
+    elif current_state == EventRegistrationForm.company:
+        user_data["company"] = m.text
+    elif current_state == EventRegistrationForm.category:
+        user_data["category"] = m.text
+    elif current_state == EventRegistrationForm.start_date:
+        try:
+            date_time = datetime.strptime(m.text, '%d-%m-%Y %H:%M').isoformat()
+        except ValueError:
+            date_time = datetime(2222, 12, 22, 22, 22).isoformat()
+        user_data["start_date"] = date_time
+    elif current_state == EventRegistrationForm.end_date:
+        try:
+            date_time = datetime.strptime(m.text, '%d-%m-%Y %H:%M').isoformat()
+        except ValueError:
+            date_time = datetime(2222, 12, 22, 22, 22).isoformat()
+        user_data["end_date"] = date_time
+    elif current_state == EventRegistrationForm.address:
+        user_data["address"] = m.text
     await dialog.next()
 
 
-async def on_date(m: Message, dialog: Dialog, manager: DialogManager):
-    manager.current_context().dialog_data["name"] = m.text
-    date_time = datetime.strptime(m.text, '%d-%m-%Y %H:%M').isoformat()
-    event_array.append(date_time)
+async def on_date(m: types.Message, dialog: Dialog, manager: DialogManager):
+
     await dialog.next()
 
 
-async def final_input(m: Message, dialog: Dialog, manager: DialogManager):
+async def final_input(m: types.Message, dialog: Dialog, manager: DialogManager):
     m_text_lower = m.text.lower()
-    event_array.append(1 if m_text_lower == "да" else 0 if m_text_lower == "нет" else 0)
-    await insert_event(event_array)
-    await m.answer("Регистрация окончена! Мероприятие успешно отправлено на модерацию!")
-    await dialog.switch_to(state=EventRegistrationForm.start)
+    need_pre_reg = 1 if m_text_lower == "да" else 0
+
+    event_arr = [
+        manager.current_context().dialog_data.get("event_name", ""),
+        manager.current_context().dialog_data.get("description", ""),
+        manager.current_context().dialog_data.get("age_restrictions", ""),
+        manager.current_context().dialog_data.get("company", ""),
+        manager.current_context().dialog_data.get("category", ""),
+        manager.current_context().dialog_data.get("start_date", ""),
+        manager.current_context().dialog_data.get("end_date", ""),
+        manager.current_context().dialog_data.get("address", ""),
+        need_pre_reg,
+    ]
+
+    await insert_event(event_arr)
+    await manager.done()
+    await reg_new(m)
 
 
 async def go_back(c: CallbackQuery, button: Button, manager: DialogManager):
@@ -132,14 +164,14 @@ dialog = Dialog(
     Window(
         Const("Время начала мероприятия в формате DD-MM-YYYY HH:MM"),
         Button(Const("Назад"), id="backStart", on_click=go_back),
-        MessageInput(on_date),
+        MessageInput(on_input),
         state=EventRegistrationForm.start_date,
         preview_add_transitions=[Next()],
     ),
     Window(
         Const("Время окончания мероприятия в формате DD-MM-YYYY HH:MM"),
         Button(Const("Назад"), id="backStart", on_click=go_back),
-        MessageInput(on_date),
+        MessageInput(on_input),
         state=EventRegistrationForm.end_date,
         preview_add_transitions=[Next()],
     ),
@@ -161,11 +193,29 @@ dialog = Dialog(
 
 registry.register(dialog)
 
+kb = [[
+    types.KeyboardButton(text="Создать новое мероприятие"),
+],
+]
 
-@dp.message_handler(commands=["start"])
-async def start(m: Message, dialog_manager: DialogManager):
+
+@dp.message_handler(Text(equals="Создать новое мероприятие"))
+async def create_new_event(message: types.Message, dialog_manager: DialogManager):
     await dialog_manager.start(EventRegistrationForm.start, mode=StartMode.RESET_STACK)
 
 
+@dp.message_handler(commands=["start"])
+async def cmd_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=kb, one_time_keyboard=True)
+    await message.answer("Привет! Для добавления нового мероприятия нажми на кнопку.",
+                         reply_markup=keyboard)
+
+
+async def reg_new(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, keyboard=kb, one_time_keyboard=True)
+    await message.answer("Регистрация окончена! Мероприятие успешно отправлено на модерацию!", reply_markup=keyboard)
+
+
 if __name__ == '__main__':
+    from aiogram import executor
     executor.start_polling(dp, skip_updates=True)
