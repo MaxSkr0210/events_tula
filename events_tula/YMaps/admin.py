40d169cf655f97001bae343198582e290ef9f835
MaxSkr0210 <maxskr0210@mail.ru>
2023-10-13 21:19:31 +0300

    add permission to delete evnts

--- events_tula/YMaps/admin.py
+++ events_tula/YMaps/admin.py
@@ -9,7 +9,7 @@ class EventsAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
     def has_delete_permission(self, request, obj=None):
-        return False
+        return True
 
 @admin.register(Users)
 class UsersAdmin(admin.ModelAdmin):
